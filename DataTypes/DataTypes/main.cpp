e76950c5bcf22c14e6545c9018fa2f665aa1e5c5
Clayman <Clayman@DESKTOP-DM8E77U>
2021-04-10 14:38:24 +0300

    Initialization of variables

--- DataTypes/DataTypes/main.cpp
+++ DataTypes/DataTypes/main.cpp
@@ -20,5 +20,33 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "Russian");
-	cout << (bool)0.0001 << "\n";
+	//cout << (bool)0.0001 << "\n";
+	/*cout << sizeof(long long ) << endl;
+	cout << INT_MIN << "\t" << INT_MAX << endl;
+	cout << LLONG_MIN << "\t" << LLONG_MAX << endl;
+	cout << ULLONG_MAX << endl;*/
+
+	///////////////////////////////////////
+	//float  - вещественное число одинарной точности
+	//double - вещественное число двойной точности
+	cout << "float:\n";
+	cout << sizeof(float) << endl;
+	cout << FLT_MIN << "\t" << FLT_MAX << endl;
+	cout << "double:\n";
+	cout << sizeof(double) << endl;
+	cout << DBL_MIN << "\t" << DBL_MAX << endl;
+	/////////////////////////////////////////////
+	//ИДЕНТИФИКАТОР - ЭТО ИМЯ!!!
+	//IDENTIFIER - ЭТО ИМЯ!!!
+	int Place1;	//Объявление переменной. В ней хранится мусор.
+	Place1 = 1;	//Инициализация переменной
+	cout << Place1 << endl;
+	double price_of_coffee;
+	int number_of_cups;
+	cout << "Введите стоимость одной чашки: ";
+	cin >> price_of_coffee;
+	cout << "Введите количество чашек: ";
+	cin >> number_of_cups;
+	double total_price = price_of_coffee * number_of_cups;
+	cout << "Общая стоимость: " << total_price << endl;
 }
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,6 +1,9 @@
 https://github.com/okovtun/SBU_121.git
 https://www.youtube.com/watch?v=stz7VJSOFQU&list=PLeqyOOqxeiIMpqE-EOx0TxG64nfHbjTiD&index=1
 
+TOREAD:
+https://github.com/okovtun/SBU_121/blob/master/DataTypes/DataTypes.pdf
+
 TODO:
 1. Выучить теорию!!!!!!!!!!!!!!!!!!!
 2. Вывести в консоль стишок "В лесу родилась Ёлочка". Стишок должен быть выровнен по середине окна консоли, и куплеты должны быть разделены пустой строкой.
