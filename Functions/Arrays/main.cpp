f0c386d1e9bf6583e2b74a162abdb0d30d2ff0d0
Clayman <Clayman@DESKTOP-DM8E77U>
2021-07-10 12:33:04 +0300

    2D array functions start.

--- Functions/Arrays/main.cpp
+++ Functions/Arrays/main.cpp
@@ -18,18 +18,37 @@ HomeWork:
 
 #define tab "\t"
 
+int g_a;//Глобальная переменная, ее видят все функции, и ее может изменить любая функция.
+		//Поэтому использование глобальных переменных настоятельно НЕ ремендуется (DEPRECATED),
+		//поскольку неизвестно что в ней хранится.
+		//g_ - Global (Hungarian notation - Венгерская нотация)https://ru.wikipedia.org/wiki/%D0%92%D0%B5%D0%BD%D0%B3%D0%B5%D1%80%D1%81%D0%BA%D0%B0%D1%8F_%D0%BD%D0%BE%D1%82%D0%B0%D1%86%D0%B8%D1%8F
+//НО, глобальные константы используются очень широко. Константу изменить никто НЕ может, но все (функции) ее видят, и могут прочитать.
+
+const int ROWS = 5;
+const int COLS = 8;
+
 void FillRand(int arr[], const int n);
 void FillRand(double arr[], const int n);
+void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS);	//Function declaration - Объявление функции (Прототип функции)
+
 void Print(int arr[], const int n);
 void Print(double arr[], const int n);
+void Print(int arr[ROWS][COLS], const int ROWS, const int COLS);
+
 void PrintReverse(int arr[], const int n);
 void PrintReverse(double arr[], const int n);
+
 int  Sum(int arr[], const int n);
 double  Sum(double arr[], const int n);
+
 double Avg(int arr[], const int n);
 double Avg(double arr[], const int n);
+
 int minValueIn(int arr[], const int n);
 int maxValueIn(int arr[], const int n);
+
+#define ARRAYS_1
+#define ARRAYS_2
 
 void main()
 {
@@ -51,6 +70,11 @@ void main()
 	cout << "Сумма элементов массива: " << Sum(d_arr, n) << endl;
 	cout << "Среднее арифметическое элементов массива: " << Avg(d_arr, n) << endl;
 	//Function overloading
+	cout << "\n======================= 2D arrays =============================\n";
+	
+	int i_arr_2[ROWS][COLS];
+	FillRand(i_arr_2, ROWS, COLS);
+	Print(i_arr_2, ROWS, COLS);
 }
 
 void FillRand(int arr[], const int n)
@@ -68,6 +92,18 @@ void FillRand(double arr[], const int n)
 		arr[i] = double(rand()%100)/10;
 	}
 }
+void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS)	//Function definition - Определение функции (Реализация функции)
+{
+	//Определяет, что делает функция.
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+}
+
 void Print(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
@@ -84,6 +120,18 @@ void Print(double arr[], const int n)
 	}
 	cout << endl;
 }
+void Print(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+}
+
 void PrintReverse(int arr[], const int n)
 {
 	for (int i = n - 1; i >= 0; i--)
